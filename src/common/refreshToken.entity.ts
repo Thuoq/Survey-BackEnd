74a8e1882ebb502bee94b901d7c4cd5fd3164d6d
JohnCowNut <thuonghamno3xz@gmail.com>
2022-04-06 20:52:21 +0700

    delete import dont need

--- src/common/refreshToken.entity.ts
+++ src/common/refreshToken.entity.ts
@@ -1,16 +1,15 @@
 import { OneToOne } from 'typeorm';
 import { User } from './../user/user.entity';
 import { Versioning } from './version.entity';
-import { BeforeInsert, Column } from 'typeorm';
+import {  Column } from 'typeorm';
 import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-import {hash} from 'bcrypt'
+
 @Entity()
 export class RefreshToken  {
   @PrimaryGeneratedColumn('uuid') id: string;
   @Column() refreshToken: string;
   @Column(type => Versioning) 
   versioning: Versioning;
- 
   @OneToOne(type => User,(user:User) =>   user.refreshToken , {onUpdate:"CASCADE"})
   user: User;
 }
